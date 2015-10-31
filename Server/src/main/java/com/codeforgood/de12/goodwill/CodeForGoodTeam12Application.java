08f9a20e4783426547b392ab8b7f37038ad8ca4d
Gregory Furlong <gnfurlong@gmail.com>
2015-10-30 21:03:46 -0400

    Initial commit. Created SpringBoot Project.

---
+++ Server/src/main/java/com/codeforgood/de12/goodwill/CodeForGoodTeam12Application.java
@@ -0,0 +1,12 @@
+package com.codeforgood.de12.goodwill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeForGoodTeam12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeForGoodTeam12Application.class, args);
+    }
+}
---
+++ Server/src/test/java/com/codeforgood/de12/goodwill/CodeForGoodTeam12ApplicationTests.java
@@ -0,0 +1,18 @@
+package com.codeforgood.de12.goodwill;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CodeForGoodTeam12Application.class)
+@WebAppConfiguration
+public class CodeForGoodTeam12ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
